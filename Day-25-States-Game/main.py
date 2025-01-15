5b1f35e8df0aaf528ff635d939652a035e2f7be8
Uriah Windham <uswindham99@gmail.com>
2025-01-15 05:24:52 -0600

    Day 25 States Game, exporting missing states to csv feature

--- Day-25-States-Game/main.py
+++ Day-25-States-Game/main.py
@@ -11,9 +11,15 @@ fill_state.speed("fastest")
 fill_state.hideturtle()
 
 correct_answers = []
+missing_answers = []
 
 x = 0
 y = 0
+
+
+all_states = pandas.read_csv("./50_states.csv")
+states_dict = all_states.to_dict("dict")
+
 
 def write_answer():
     fill_state.goto(x, y)
@@ -21,17 +27,25 @@ def write_answer():
 
 game_on = True
 while game_on:
-    answer_state = screen.textinput(title=f"{len(correct_answers)}/50 States Guessed", prompt="What's another state's name?").title()
-    all_states = pandas.read_csv("./50_states.csv")
-    states_dict = all_states.to_dict("dict")
+    answer_state = screen.textinput(title=f"{len(correct_answers)}/50 States Guessed",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
+
     for key, val in states_dict["state"].items():
         if val == answer_state:
             x = states_dict["x"][key]
             y = states_dict["y"][key]
             write_answer()
             correct_answers.append(answer_state)
+
     if len(correct_answers) == 50:
         game_on = False
         screen.bye()
 
-screen.mainloop()
+for key, val in states_dict["state"].items():
+    if val not in correct_answers:
+        missing_answers.append(val)
+
+pandas.Series(missing_answers).to_csv("./missing_states.csv")
